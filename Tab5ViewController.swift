4c9d88a3371d7e3c8a2cc49cfb05f408a31d2abd
steve <vnsgbt@me.com>
2016-02-18 12:58:45 -0600

    Fix storyboard and add extension

---
+++ Tab5ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Tab5ViewController.swift
+//  iOS-Five-tabs-starter
+//
+//  Created by dev2 on 2/18/16.
+//  Copyright © 2016 vnsgbt. All rights reserved.
+//
+
+import UIKit
+
+class Tab5ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS-Five-tabs-starter/UIColor+Tabs.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Tabs.swift
+//  iOS-Five-tabs-starter
+//
+//  Created by dev2 on 2/18/16.
+//  Copyright © 2016 vnsgbt. All rights reserved.
+//
+
+import Foundation
